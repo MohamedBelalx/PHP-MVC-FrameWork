194ef1c7158763f76254c6d514adcedfe1b738e7
MohamedBelalx <hero.armna@gmail.com>
2019-09-15 17:21:11 +0200

    Add DB Class And Test Connection

--- app/config/config.php
+++ app/config/config.php
@@ -5,4 +5,9 @@ define('APPROOT',dirname(dirname(__FILE__)));
 define('URLROOT','http://localhost/MVC');
 
 define('SITENAME','CHATAPP');
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','cms');
 ?>
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,10 +1,11 @@
 <?php
 
-class Pages
+class Pages extends Controller
 {
     public function __construct()
     {
         echo 'pages loded';
+        $this->PostModel = $this->model('Post');
     }
     public function index()
     {
