5d0c14526eb004b43b72c5067fe7a61a9449d37a
MohamedBelalx <hero.armna@gmail.com>
2019-09-15 14:33:36 +0200

    Make Config File And Auto Loader

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,9 +1,15 @@
 <?php
 
+require('config/config.php');
+/*
 require('libraries/Core.php');
 require('libraries/Controller.php');
 require('libraries/Database.php');
+*/
+spl_autoload_register(function($className)
+{
+    require('libraries/'.$className.'.php');
 
-
+});
 
 ?>
---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('APPROOT',dirname(dirname(__FILE__)));
+
+define('URLROOT','http://localhost/MVC');
+
+define('SITENAME','CHATAPP');
+?>
